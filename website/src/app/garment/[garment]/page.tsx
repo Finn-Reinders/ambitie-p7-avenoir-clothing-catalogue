5853c252b0e3bd30ff7266295e6b692caba76343
Finn-Reinders <finn.reinders@sintlucasedu.nl>
2026-03-30 14:24:51 +0200

    removed error

--- website/src/app/garment/[garment]/page.tsx
+++ website/src/app/garment/[garment]/page.tsx
@@ -5,7 +5,6 @@ import { garmentData } from '../../../modules/garmentsData';
 
 export default function garment() {
   const { garment } = useParams();
-  const garmentData = garmentData.find((p) => p.garment === garment);
   return (
     <Page>
       <div className='w-screen h-screen'>{garment}</div>

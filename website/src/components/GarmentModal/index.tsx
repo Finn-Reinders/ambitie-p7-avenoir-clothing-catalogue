6c11c839e438fbc5d1f04fe869c4f11d4078b1b5
DanielBovee <d.bovee@sintlucasedu.nl>
2026-04-02 11:55:42 +0200

    Removed unused folder and file

--- website/src/components/GarmentModal/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function GarmentModal() {
-  return (
-    <div className="w-[50vw] h-[50vh] absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]  text-white bg-black"></div>
-  )
-}
